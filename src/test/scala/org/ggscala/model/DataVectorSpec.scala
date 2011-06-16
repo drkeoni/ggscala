ea3b81da0adeeb040b0752fa847fb73d8d1dc942
Jon Sorenson <jon_m_sorenson@yahoo.com>
2011-06-16 10:53:24 -0700

    added additional test harness

---
+++ src/test/scala/org/ggscala/model/DataVectorSpec.scala
@@ -0,0 +1,34 @@
+package org.ggscala.model
+
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+
+import org.ggscala.model.MultiColumnSource.StringVector
+import org.ggscala.test.TestUtils
+
+class DataVectorSpec extends FlatSpec with ShouldMatchers {
+  
+  "A StringVector" should "be simple to construct" in
+  {
+    val sv = new StringVector( Array( "A", "B", "C" ) )
+    sv.toArray should equal ( Array( "A", "B", "C" ) )
+  }
+  
+  it should "support iteration" in
+  {
+    val sv = new StringVector( Array( "A", "B", "C" ) )
+    var count = 0
+    for( a <- sv ) count += 1
+    count should be (3)
+  }
+  
+}
+
+object DataVectorSpec {
+  def main(args:Array[String]) = 
+  {
+    TestUtils.timing {
+      (new DataVectorSpec).execute
+    }
+  }
+}
