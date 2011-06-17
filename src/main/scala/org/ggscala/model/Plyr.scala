8a6f4d44a624560f59a4977de37564c4084ea1e1
Jon Sorenson <jon_m_sorenson@yahoo.com>
2011-06-17 01:11:48 -0700

    Working towards implementing ddply (getting close, but taking a break)

--- src/main/scala/org/ggscala/model/Plyr.scala
+++ src/main/scala/org/ggscala/model/Plyr.scala
@@ -4,8 +4,9 @@
  */
 package org.ggscala.model
 
-import org.ggscala.model.MultiColumnSource.{MultiColumnSource,RowBindable}
+import org.ggscala.model.MultiColumnSource.{MultiColumnSource,RowBindable,IterableDataVector}
 import org.ggscala.model.Factor._
+import scala.collection.mutable.HashMap
 
 object Plyr {
   // TODO: work out the type signature for this
@@ -18,13 +19,33 @@ object Plyr {
     {
       assert( split.length >= 1 )
       // enumerate unique compound keys
+      /*
       def col(k:String) = data.$a(k).map(_.toString)
       def compound(a:Iterable[String],key:String) = (a zip col(key)).map{ case (s1,s2) => s1 + "_" + s2 }
       val compoundKeys = split.tail.foldLeft( col(split(0)) )( (a,b) => compound(a,b) ).toArray
       val compoundFactors = new FactorVector(compoundKeys)
+      */
+      val _names = data.names
+      val colIndices = split.map( _names.indexOf(_) )
+      def compoundKey( row:IndexedSeq[Any] ) = colIndices.map( row(_).toString ) mkString "_"
+      data.rowIterator.toList.groupBy(compoundKey _)
       // partition
       // map and fold
       data
     }
+  }
+  
+  class RowList( val _names:Seq[String], val rows:List[IndexedSeq[Any]] ) extends MultiColumnSource
+  {
+    val id2num = {
+      val hash = new HashMap[String,Int]
+      _names.zipWithIndex.foreach { case (v,i) => hash(v) = i }
+      hash
+    }
+    def names = _names
+    def $a( id:String ) = new IterableDataVector(rows.map( r => r(id2num(id)) ))
+    def $s( id:String ) = new IterableDataVector(rows.map( r => r(id2num(id)).toString ))
+    def $d( id:String ) = new IterableDataVector(rows.map( r => r(id2num(id)).asInstanceOf[Double] ))
+    def $f( id:String ) = new IterableDataVector(rows.map( r => r(id2num(id)).asInstanceOf[Factor] ))
   }
 }
